6a25e9e1e8fac50a2743f3903e2d91bcdff29ca1
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-27 17:14:19 +0200

    add model Watchlist

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -11,6 +13,20 @@ class StreamPlatform(models.Model):
         return self.name
     
     
+class WatchList(models.Model):
+    title = models.CharField(max_length=50)
+    storyline = models.CharField(max_length=200)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")
+    active = models.BooleanField(default=True)
+    avg_rating = models.FloatField(default=0)
+    number_rating = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    
+
     
     
     
