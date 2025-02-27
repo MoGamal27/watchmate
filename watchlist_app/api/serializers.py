2d6898d3deabadc4e21055a038d4e96a58bef43e
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2025-02-27 17:21:51 +0200

    add StreamPlatformSerializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from watchlist_app.models import Review, WatchList
+from watchlist_app.models import Review, WatchList, StreamPlatform
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -17,4 +17,13 @@ class WatchListSerializer(serializers.ModelSerializer):
     class Meta:
         model = WatchList
         fields = "__all__"
-    
+
+
+class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = StreamPlatform
+        fields = "__all__"
+        
+        
